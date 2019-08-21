9d9636ff256347fd26610763f0e9f5c9eb0fe30d
vghy顾海洋 <hy_gu@ctrip.com>
2019-08-21 22:48:25 +0800

    bio

---
+++ src/main/java/org/netty/definitive/bio/TimeClient.java
@@ -0,0 +1,37 @@
+package org.netty.definitive.bio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class TimeClient {
+
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        BufferedReader in = null;
+        PrintWriter out = null;
+
+        try (Socket socket = new Socket("127.0.0.1", port)) {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            out = new PrintWriter(socket.getOutputStream(), true);
+            out.println("QUERY TIME ORDER");
+            System.out.println("Send order 2 server succeed.");
+            String resp = in.readLine();
+            System.out.println("Now is : " + resp);
+        } catch (Exception e) {
+
+        } finally {
+            if (out != null) {
+                out.close();
+                out = null;
+            }
+
+            if (in != null) {
+                in.close();
+                in = null;
+            }
+        }
+    }
+}
---
+++ src/main/java/org/netty/definitive/bio/TimeServer.java
@@ -0,0 +1,31 @@
+package org.netty.definitive.bio;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TimeServer {
+
+    public static void main(String[] args) throws IOException {
+        int port = 8080;
+        ServerSocket server = null;
+        try {
+            server = new ServerSocket(port);
+            System.out.println("The time server is start in port :" + port);
+            Socket socket = null;
+            while (true) {
+                socket = server.accept();
+                new Thread(new TimeServerHandler(socket)).start();
+
+                if (1 == 2) {
+                    break;
+                }
+            }
+        } finally {
+            if (server != null) {
+                server.close();
+                server = null;
+            }
+        }
+    }
+}
